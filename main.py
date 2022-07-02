1c6f5c46e4e359a8bbde434b80ae52a1a2aa52cb
unknown <michaelvin1322@gmail.com>
2022-07-02 18:40:40 +0300

    Declared get_word function

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
 if __name__ == "__main__":
     print("hello world!")
---
+++ tools.py
@@ -0,0 +1,6 @@
+def get_word(word):
+    """
+    :param word: string which contains a French verb
+    :return: Word object
+    """
+    pass
